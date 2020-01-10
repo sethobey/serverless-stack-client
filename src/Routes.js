c3d9644907732fe9b429fa35806416197e900052
Seth Obey <sethobey@gmail.com>
2020-01-10 11:16:33 -0600

    Add note view

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from './containers/Signup';
+import Notes from "./containers/Notes";
 import NewNote from "./containers/NewNote";
 import NotFound from './containers/NotFound';
 import AppliedRoute from './components/AppliedRoute';
@@ -13,6 +14,7 @@ export default function Routes({ appProps }) {
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
       <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
       <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
